f31d87df03a69a037dc6a50c36715dce0aca042f
tekart <87260264+tekarrt@users.noreply.github.com>
2022-01-16 01:54:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0){
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(params){
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemoffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if((pageYOffset > animItemoffset - animItemPoint) && pageYOffset < (animItemoffset + animItemHeight)){
+                animItem.classList.add('_active');
+            }
+        }
+    }
+    function offset(el) {
+    const rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+   }
+};
+
+ 
+
+
